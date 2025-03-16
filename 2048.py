2577d85ce88553e81d384939bce1a12dec9d35ab
Yacine <yboulachgour@gmail.com>
2025-03-16 22:10:42 +0100

    Clean code

--- 2048.py
+++ 2048.py
@@ -6,44 +6,11 @@
 #Namsai Souvanthong
 
 #### MODULES / BIBLIOTHEQUES 
-
 import tkinter as tk
 import random as rd
-
-taille_case = 100
-
-#### PARTIE INTERFACE #####
-
 import tkinter as tk
 
-
-fenetre = tk.Tk()
-fenetre.title('2048')
-label_title = tk.Label(fenetre, text = "2048", font = ("Arial", 20), bg = "#df9d97", fg = "white")
-label_title.pack()
-
-fenetre.geometry("800x800") 
-
-
-canvas = tk.Canvas(fenetre, width=395, height=395, borderwidth=2, relief="solid")
-
-taille_case = 100
-liste_carre = []
-
-def draw(): 
-    """Dessiner le carré avec des dimensions de taille_casextaille_case pixels pour chaque case"""
-    for y in range(4):
-        for x in range(4):
-            canvas.create_rectangle(x*taille_case, y*taille_case, (y+1)*taille_case, (x+1)*taille_case)
-            text_carre = tk.Label(canvas, font = ("Arial",64),fg = "white")
-            liste_carre.append({
-                "x":x,
-                "y":y,
-                "valeur":0,
-                "label": text_carre
-            })
-            text_carre.place(x=x * taille_case + 1, y=y * taille_case + 1, width=taille_case -2 , height=taille_case -2)
-
+TAILLE_CASE = 100
 
 def gauche():
     decale = True
@@ -98,12 +65,27 @@ def update_labels():
             carre["label"].config(text = str(""))
         else:
             carre["label"].config(text = str(carre["valeur"]))
-# permet d'afficher 
+
+# permet d'initialiser le jeu
 def start():
-    draw()
+    init_grid()
     get_random_free_cell()["valeur"] = 2
     get_random_free_cell()["valeur"] = 2
     update_labels()
+
+# permet de générer la grille
+def init_grid():
+    """Dessiner le carré avec des dimensions de taille_case*taille_case pixels pour chaque case"""
+    for y in range(4):
+        for x in range(4):
+            canvas.create_rectangle(x * TAILLE_CASE, y * TAILLE_CASE, (y+1) * TAILLE_CASE, (x+1) * TAILLE_CASE)
+            text_carre = tk.Label(canvas, font = ("Arial",64),fg = "white")
+            liste_carre.append({
+                "valeur": 0,
+                "label": text_carre
+            })
+            text_carre.place(x=x * TAILLE_CASE + 1, y=y * TAILLE_CASE + 1, width=TAILLE_CASE -2 , height=TAILLE_CASE -2)
+
 
 #effectue un tour de jeux 
 def turn():
@@ -113,34 +95,37 @@ def turn():
     update_labels() #mise à jour d'affichage 
 
 
+fenetre = tk.Tk()
+fenetre.title('2048')
+label_title = tk.Label(fenetre, text = "2048", font = ("Arial", 20), bg = "#df9d97", fg = "white")
+label_title.pack()
+
+fenetre.geometry("800x800") 
+
+
+canvas = tk.Canvas(fenetre, width=395, height=395, borderwidth=2, relief="solid")
+liste_carre = []
 
 # Boutons
-left_button = tk.Button(fenetre, text="←", fg = "black", font=("Arial","15"),command= gauche)  #command=gauche
-right_button = tk.Button(fenetre, text="→", fg = "black", font=("Arial","15"),command = droite) #command=droite
-up_button = tk.Button(fenetre, text="↑",  fg = "black", font=("Arial","15"),command=haut) #command=haut
-down_button = tk.Button(fenetre, text="↓", fg = "black", font=("Arial","15"),command=bas)#command=bas
+left_button = tk.Button(fenetre, text="←", fg = "black", font=("Arial","15"),command= gauche)
+right_button = tk.Button(fenetre, text="→", fg = "black", font=("Arial","15"),command = droite)
+up_button = tk.Button(fenetre, text="↑",  fg = "black", font=("Arial","15"),command=haut)
+down_button = tk.Button(fenetre, text="↓", fg = "black", font=("Arial","15"),command=bas)
 
-left_button.place(relx=0.80, rely=0.5, anchor="center")   # command = gauche
-right_button.place(relx=0.90, rely=0.5, anchor="center")  # command = droite
-up_button.place(relx=0.85, rely=0.42, anchor="center")    # command = haut
-down_button.place(relx=0.85, rely=0.58, anchor="center")  #command = bas
-
-
-start()
+left_button.place(relx=0.80, rely=0.5, anchor="center")
+right_button.place(relx=0.90, rely=0.5, anchor="center")
+up_button.place(relx=0.85, rely=0.42, anchor="center")
+down_button.place(relx=0.85, rely=0.58, anchor="center")
 
 # Ajouter le canevas à la fenêtre
 canvas.pack(expand=True)
 
-liste_carre_disponible = [[i,j,0] for i in range(4) for j in range(4)]
-
 #Initailisation du score
 score = 0
-check = 0
-#List stockant les carrés
 
 #afficher label score 
-#Afficher le score
 score_label = tk.Label(fenetre, text="Score : " + str(score), font = ("helvetica", "20"))
 score_label.pack()
+start()
 
 fenetre.mainloop()
